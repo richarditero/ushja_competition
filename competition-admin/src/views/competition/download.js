80afb4b6f8b2a2a682a7dfb58917207e83dc0190
vignesh <p.vigneshwaran@itero.ie>
2020-12-15 14:29:16 +0530

    export file changes

--- competition-admin/src/views/competition/download.js
+++ competition-admin/src/views/competition/download.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import _ from 'lodash';
-import ReactExport from 'react-data-export';
-import { Button } from '@material-ui/core';
-import ApiUtil from '../../util/ApiUtil';
+import React, { useState } from "react";
+import _ from "lodash";
+import ReactExport from "react-data-export";
+import { Button } from "@material-ui/core";
+import ApiUtil from "../../util/ApiUtil";
 
 const { ExcelFile } = ReactExport;
 const { ExcelSheet } = ReactExport.ExcelFile;
@@ -14,12 +14,36 @@ export default function Download({ handlePageLoder }) {
   const exportToExcel = () => {
     handlePageLoder(true);
     ApiUtil.getWithToken(
-      'open-competition?orderby=competitionName&order=ASC',
+      "open-competition?orderby=competitionName&order=ASC"
     ).then((result) => {
       if (result.status === 200) {
         const data = result.data.rows;
-        const arr = [];
-        data.map((d) => {
+        let arr = [];
+        console.log(data);
+
+        for (let i = 0; i < data.length; i++) {
+          let temp = data[i].open_competition_purchaseds.map((val) => {
+            return {
+              competitionName: data[i].competitionName,
+              username: data[i].username,
+              userEmail: data[i].userEmail,
+              compFormName: data[i].compFormName,
+              transactionId: data[i].transactionId,
+              event: val.name,
+              eventTotal: val.amount,
+              total: data[i].amount,
+              horses:val.quantity,
+              competitionDate: data[i].competitionDate,
+              location: data[i].location,
+              status: data[i].status,
+              card:data[i].cardType
+            };
+          });
+          arr = [...arr, ...temp];
+        }
+        console.log(arr);
+
+        /*         data.map((d) => {
           arr.push({
             competitionName: d.competitionName,
             username: d.username,
@@ -32,7 +56,7 @@ export default function Download({ handlePageLoder }) {
             events: _.map(d.open_competition_purchaseds, 'name').join(' ,'),
             status: d.status,
           });
-        });
+        }); */
         handlePageLoder(false);
         setExcelData(arr);
         setExcelData(null);
@@ -47,7 +71,7 @@ export default function Download({ handlePageLoder }) {
         color="primary"
         variant="contained"
         style={{
-          height: 'fit-content',
+          height: "fit-content",
           fontWeight: 600,
         }}
         onClick={() => exportToExcel()}
@@ -58,19 +82,19 @@ export default function Download({ handlePageLoder }) {
         <ExcelFile
           hideElement
           filename="CompetitionTable"
-          element={(
+          element={
             <Button
               color="primary"
               variant="contained"
               style={{
-                height: 'fit-content',
+                height: "fit-content",
                 fontWeight: 600,
               }}
               onClick={() => exportToExcel()}
             >
               Export
             </Button>
-          )}
+          }
         >
           <ExcelSheet data={excelData} name="Payment Details">
             <ExcelColumn label="Competition Name" value="competitionName" />
@@ -78,10 +102,13 @@ export default function Download({ handlePageLoder }) {
             <ExcelColumn label="Email" value="userEmail" />
             <ExcelColumn label="Competition Form" value="compFormName" />
             <ExcelColumn label="Transaction Id" value="transactionId" />
-            <ExcelColumn label="Amount" value="amount" />
             <ExcelColumn label="Competition Date" value="competitionDate" />
-            <ExcelColumn label="Events" value="events" />
+            <ExcelColumn label="Event Name" value="event" />
+            <ExcelColumn label="Event Total" value="eventTotal" />
+            <ExcelColumn label="Total" value="total" />
+            <ExcelColumn label="Horses" value="horses" />
             <ExcelColumn label="Location" value="location" />
+            <ExcelColumn label="Card" value="card" />
             <ExcelColumn label="Status" value="status" />
           </ExcelSheet>
         </ExcelFile>
